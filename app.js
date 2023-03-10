06cf6f8d53368063183b9f0b4a27c5d7b3ccdfad
joey-00 <joseacevedo410@gmail.com>
2023-03-10 06:38:50 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+jose@DESKTOP-AA8CDLT MINGW64 ~/Desktop
+$ git clone https://github.com/joey-00/bajando-archivos.git
+Cloning into 'bajando-archivos'...
+warning: You appear to have cloned an empty repository.
+
+jose@DESKTOP-AA8CDLT MINGW64 ~/Desktop
+$ cd bajando-archivos
+
+jose@DESKTOP-AA8CDLT MINGW64 ~/Desktop/bajando-archivos (master)
+$ git status
+On branch master
+
+No commits yet
+
+nothing to commit (create/copy files and use "git add" to track)
+
+jose@DESKTOP-AA8CDLT MINGW64 ~/Desktop/bajando-archivos (master)
+$ git add app.js
+
+
+
+jose@DESKTOP-AA8CDLT MINGW64 ~/Desktop/bajando-archivos (master)
+$ git status
+On branch master
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        app.js
+
+nothing added to commit but untracked files present (use "git add" to track)
+
